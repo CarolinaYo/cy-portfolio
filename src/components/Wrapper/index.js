9829ead696e6c1de5246ab4cdc62063c05bf3715
CarolY <mon_alisa75@yahoo.com>
2021-02-08 09:36:26 -0800

    Rewrite path to background image.

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./style.css";
-// import background from "../Images/background";
 
 function Wrapper(props) {
   return <main className="wrapper">{props.children}</main>;
